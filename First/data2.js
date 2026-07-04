b063d6a5eacf780f94c80f9d4832ca7242f213c0
ICEguy45 <saisumanthpasumarthi489@gmail.com>
2026-07-04 11:53:33 +0000

    another one

---
+++ First/data2.js
@@ -0,0 +1,10 @@
+let score = 33
+
+// console.log("2">1)
+// console.log("02">1)
+// This is rather stupid never compare two different data types bad coding practice
+
+console.log(null>0)
+console.log(null==0)
+console.log(null<=0)
+console.log(null<0)
