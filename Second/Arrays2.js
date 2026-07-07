7617e3dc1347888e48a2b38851c34a0c48bd41eb
ICEguy45 <saisumanthpasumarthi489@gmail.com>
2026-07-07 16:42:29 +0000

    Done

---
+++ Second/Arrays2.js
@@ -0,0 +1,19 @@
+const marvel_heros = ["thor","spider-man","Iron-man"]
+const DC_heros = ["Batman","flash","Super-man"]
+
+// marvel_heros.push(DC_heros)
+
+// console.log(marvel_heros)
+
+// new_heros = marvel_heros.concat(DC_heros)
+
+// console.log(new_heros)
+
+const new_heros = [...marvel_heros,...DC_heros]
+console.log(new_heros)
+
+const another_array = [1,2,4,5,6,[6,7],[6,7,[10,4]],1]
+
+const real_another_array = another_array.flat(Infinity)
+
+console.log(real_another_array)
