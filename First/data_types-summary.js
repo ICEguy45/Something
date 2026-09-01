73690430803a79f3435cc9070273ef19cb1cf8c6
ICEguy45 <saisumanthpasumarthi489@gmail.com>
2026-09-01 15:54:02 +0000

    Functions and changes

--- First/data_types-summary.js
+++ First/data_types-summary.js
@@ -3,10 +3,24 @@
 
 // Primitive: String,Number,Boolean,undefined,Null,Symbol,BigInt
 
-const id = Symbol("123")
-const another = Symbol("123")
+// const id = Symbol("123")
+// const another = Symbol("123")
 
-console.log(id == another)
+// console.log(id == another)
 
-const BigNumber = 21333314n
-console.log(typeof BigNumber)
+// const BigNumber = 21333314n
+// console.log(typeof BigNumber)
+
+// const arrow_function = (a,b) => {
+//   return a + b;
+// }
+// console.log(arrow_function(1,2))
+// const rgbColor = [255, 128, 0]; 
+// const [r, g, b] = rgbColor; 
+// console.log(r); // Output: 255 
+// console.log(g); // Output: 128 
+// console.log(b); // Output: 0 
+const frontendSkills = ["HTML", "CSS", "JavaScript"]; 
+const fullStackSkills = [...frontendSkills, "React.js", "Node.js"]
+
+console.log(fullStackSkills)
---
+++ Third/functions_and_objects.js
@@ -0,0 +1,10 @@
+const user = {
+  username: "hitesth",
+  price: 200
+}
+
+function handleObject(anyobject){
+  console.log(`The username is ${anyobject.username} and the price is ${anyobject.price}`)
+}
+
+handleObject(user)
