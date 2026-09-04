49644c2ccb8806091c1656925cb28e539ef94559
ICEguy45 <saisumanthpasumarthi489@gmail.com>
2026-09-04 08:44:41 +0000

    IIFE_new

--- Third/IIFE.js
+++ Third/IIFE.js
@@ -2,7 +2,7 @@
   console.log(`DB CONNECTED`);
 })();
 
-( function aurcode() {
-  console.log(`DB CONNECTED TWO`);
-} )()
+( (name) => {
+  console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
 
